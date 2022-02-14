6525cc9d4ff2cad1bc73911da56d0734f0dba79d
WallyWest74422 <53949174+WallyWest74422@users.noreply.github.com>
2022-02-13 16:26:55 -0800

    twccccaa

--- src/_08_California_Weather/WeatherFinder.java
+++ src/_08_California_Weather/WeatherFinder.java
@@ -1,89 +1,27 @@
 package _08_California_Weather;
 
-//import java.io.BufferedReader;
-//import java.io.FileReader;
-//import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
 
-//import javax.swing.JOptionPane;
+import javax.swing.JOptionPane;
 
-//public class WeatherFinder {
-//	private final static String caliWeatherFilename = "california_cities_weather.txt";
-//public static String main(String[] args) {
-//String start() {
-//	Utilities u = new Utilities();
-//String cityName =	JOptionPane.showInputDialog("Enter California city name");
-//// u.capitalizeWords( cityName );{
-//    String formattedCityName = "";
-    
- //   String[] cityNameWords = cityName.split(" ");
-//    for( String word : cityNameWords ) {
-//        formattedCityName += word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase() + ' ';
-//    }
-//    
- //   return formattedCityName.trim();
-//}
- //HashMap<String, WeatherData> getWeatherData;(cityName); {
-//     HashMap<String, WeatherData> weatherData = new HashMap<>();
-//     HashMap<String, Integer> duplicateLocations = new HashMap<>();
-     
-//     try {
-//         BufferedReader br = new BufferedReader(new FileReader(caliWeatherFilename));
-         
-//         String line = br.readLine();
-//         while(line != null){
-             
-             // Format of each file entry is:
-             // <city name>, <weather summary>, <temperature degrees F>
- //            String[] datum = line.split(", ");
-             
- //            if( datum.length > 2 ) {
-  //               StringBuilder cityName = new StringBuilder( datum[0] );
- //                String weatherSummary = datum[1];
-  //               double temperatureF = Double.parseDouble( datum[2] );
-             
-                 // Add a separate entry for duplicate city names because
-                 // large cities have multiple recordings
-   //              if( weatherData.containsKey(cityName.toString()) ) {
-//                     if( duplicateLocations.containsKey(cityName.toString()) ){
-//                         Integer duplicateNum = duplicateLocations.get(cityName.toString());
-                     
-                         // Update number at the end, e.g. Orange1, Orange2, ...
-///                         duplicateNum++;
-//                         duplicateLocations.put(cityName.toString(), duplicateNum);
-                     
-                         // Only do this AFTER updating the duplicate num suffix
-   //                      cityName.append( duplicateNum );
-  //                   } else {
-//                         duplicateLocations.put(cityName.toString(), 1);
-    //                     cityName.append(1);
-  //                   }
-  //               }
-                 
- //                weatherData.put(cityName.toString(), new WeatherData(weatherSummary, temperatureF));
-//             }
-             
-             // Read next entry
-//             line = br.readLine();
-//         }
-         
- //        br.close();
- //    } catch(Exception e) {
- //        e.printStackTrace();
- //    }
-     
-//     return weatherData;
-// }
-// HashMap<String, WeatherData> weatherData = u.getWeatherData();
-// WeatherData datum = weatherData.get(cityName);
- 
- //if( datum == null ) {
-//     System.out.println("Unable to find weather data for: " + cityName);
-// } else {
-//     System.out.println(cityName + " is " + datum.weatherSummary + " with a temperature of " + datum.temperatureF + " F");
-// }
-
-
-
-//}
-//}
-//}
+public class WeatherFinder {
+	HashMap start() {
+        HashMap<String, WeatherData> weatherData = Utilities.getWeatherData();
+        String cityName = JOptionPane.showInputDialog("Which city in California would you like to see the weather of?");
+ String cityNameNew = Utilities.capitalizeWords(cityName ); {
+}
+    		  WeatherData datum = weatherData.get(cityNameNew);
+        
+        if( datum == null ) {
+            System.out.println("Unable to find weather data for: " + cityName);
+        } else {
+            System.out.println(cityName + " is " + datum.weatherSummary + " with a temperature of " + datum.temperatureF + " F");
+        }
+        String Condition = JOptionPane.showInputDialog("Type in a weather condition in order to see the cities in California that match that description.");
+        HashMap<String, WeatherData> city = Utilities.getWeatherData();
+	return weatherData;
+	
+}
+}
