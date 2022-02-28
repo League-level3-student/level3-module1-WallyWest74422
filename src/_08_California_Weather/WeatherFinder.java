058b0f16de80d4ce8580e764b04d9fec401dffb7
WallyWest74422 <53949174+WallyWest74422@users.noreply.github.com>
2022-02-27 16:28:54 -0800

    ccccccccc

--- src/_08_California_Weather/WeatherFinder.java
+++ src/_08_California_Weather/WeatherFinder.java
@@ -2,11 +2,15 @@ package _08_California_Weather;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.swing.JOptionPane;
 
+
 public class WeatherFinder {
+	 ArrayList<String> matchingCities = new ArrayList<String>();
+	 ArrayList<String> inBetweenCities = new ArrayList<String>();
 	HashMap start() {
         HashMap<String, WeatherData> weatherData = Utilities.getWeatherData();
         String cityName = JOptionPane.showInputDialog("Which city in California would you like to see the weather of?");
@@ -23,16 +27,27 @@ public class WeatherFinder {
         String condition = JOptionPane.showInputDialog("Type in a weather condition in order to see the cities in California that match that description.");
         String conditionNew = Utilities.capitalizeWords(condition);
         for(String city : cityName1.keySet()){
-          if( cityName1.get(city).weatherSummary==conditionNew) {
-        	  
+          if( cityName1.get(city).weatherSummary.equals(conditionNew)) {
+        	  matchingCities.add(city);
           }else {
-        	  cityName1.remove(city, cityName1.get(city));
+ //       	  System.out.println("Unable to find cities for" + conditionNew + ".");
           }
-          System.out.println(cityName1.values());
+          for(String s : matchingCities){
+              System.out.println(s);
+          }
         }
-        	System.out.println(cityName1);
-        
-
+        HashMap<String, WeatherData> inBetween = Utilities.getWeatherData();
+        String min = JOptionPane.showInputDialog("Type in a minimum temperature.");
+        int min1=Integer.parseInt(min);
+        String max = JOptionPane.showInputDialog("Type in a maximum temperature.");
+        int max1=Integer.parseInt(max);
+        for(String city1 : inBetween.keySet()){
+        	 if( inBetween.get(city1).temperatureF >min1 &&  inBetween.get(city1).temperatureF <max1) {
+           	  inBetweenCities.add(city1);
+             }for(String i : inBetweenCities) {
+        		 System.out.println(i);
+        	 }
+        }
 	return weatherData;
 
 }
