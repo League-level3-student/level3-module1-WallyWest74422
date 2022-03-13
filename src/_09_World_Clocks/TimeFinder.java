bf34d162a095a63ee5226d4cb53e18b0fadcfdf4
WallyWest74422 <53949174+WallyWest74422@users.noreply.github.com>
2022-03-13 16:18:46 -0700

    ssssssssssssssssssssssss

--- src/_09_World_Clocks/TimeFinder.java
+++ src/_09_World_Clocks/TimeFinder.java
@@ -17,7 +17,7 @@ public class TimeFinder implements ActionListener {
     ClockUtilities clockUtil;
     Timer timer;
     TimeZone timeZone0;
-
+    HashMap<String, TimeZone> cities;
     JFrame frame;
     JPanel panel;
     JTextArea textArea;
@@ -30,21 +30,32 @@ public class TimeFinder implements ActionListener {
     	clockUtil = new ClockUtilities();
     	 city0 = "San Diego, US";
          timeZone0 = clockUtil.getTimeZoneFromCityName(city0);
-         HashMap<String, TimeZone> cities = new HashMap<String, TimeZone>();
+         cities = new HashMap<String, TimeZone>();
          cities.put(city0, timeZone0);
          String city1 = JOptionPane.showInputDialog("Type in the name of a city in this format 'City, Country' in order to see its current time.");
          TimeZone timeZone1 = clockUtil.getTimeZoneFromCityName(city1);
          cities.put(city1, timeZone1);
+         String city2 = JOptionPane.showInputDialog("Type in the name of another city in this format 'City, Country' in order to see its current time.");
+         TimeZone timeZone2 = clockUtil.getTimeZoneFromCityName(city2);
+         cities.put(city2, timeZone2);
          Calendar calendar = null;
-         for (int i = 0; i < cities.size(); i++) {
-			 calendar = Calendar.getInstance(cities.get("city"+i));
-		}
+
+
+             for(String c : cities.keySet()){
+            	 
+                 calendar = Calendar.getInstance(cities.get(c));
+                 String month = calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault());
+                 String dayOfWeek = calendar.getDisplayName( Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.getDefault());
+                 dateStr = dayOfWeek + " " + month + " " + calendar.get(Calendar.DAY_OF_MONTH) + " " + calendar.get(Calendar.YEAR);
+                 System.out.println(dateStr);
+             }
+			 
     	 //Calendar calendar = Calendar.getInstance(timeZone1);
-         String month = calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault());
-         String dayOfWeek = calendar.getDisplayName( Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.getDefault());
-         dateStr = dayOfWeek + " " + month + " " + calendar.get(Calendar.DAY_OF_MONTH) + " " + calendar.get(Calendar.YEAR);
+      //   String month = calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault());
+     //    String dayOfWeek = calendar.getDisplayName( Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.getDefault());
+      //   dateStr = dayOfWeek + " " + month + " " + calendar.get(Calendar.DAY_OF_MONTH) + " " + calendar.get(Calendar.YEAR);
          
-         System.out.println(dateStr);
+         //System.out.println(dateStr);
          
          frame = new JFrame();
          panel = new JPanel();
@@ -64,14 +75,16 @@ public class TimeFinder implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-		 Calendar c = Calendar.getInstance(timeZone0);
-	        String militaryTime = c.get(Calendar.HOUR_OF_DAY) + ":" + c.get(Calendar.MINUTE) + ":" + c.get(Calendar.SECOND);
-	        String twelveHourTime = " [" + c.get(Calendar.HOUR) + ":" + c.get(Calendar.MINUTE) + ":" + c.get(Calendar.SECOND) + "]";
+		 for(String c : cities.keySet()){
+		 Calendar c2 = Calendar.getInstance(cities.get(c));
+	        String militaryTime = c2.get(Calendar.HOUR_OF_DAY) + ":" + c2.get(Calendar.MINUTE) + ":" + c2.get(Calendar.SECOND);
+	        String twelveHourTime = " [" + c2.get(Calendar.HOUR) + ":" + c2.get(Calendar.MINUTE) + ":" + c2.get(Calendar.SECOND) + "]";
 	        timeStr = militaryTime + twelveHourTime;
-	        
 	        System.out.println(timeStr);
-	        textArea.setText(city0 + "\n" + dateStr + "\n" + timeStr);
+	        textArea.setText(cities.keySet() + "\n" + dateStr + "\n" + timeStr);
 	        frame.pack();
+		 }
+	        
 	}
     
 
